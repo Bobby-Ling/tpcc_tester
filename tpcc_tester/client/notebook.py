03e27b67a050860de81e371235591578063e806e
Bobby-Ling <bobby-ling@outlook.com>
2025-08-16 07:16:12 +0800

    notebook

--- tpcc_tester/client/notebook.py
+++ tpcc_tester/client/notebook.py
@@ -11,11 +11,25 @@ sys.path.append(str(file_dir.parent.parent))
 from tpcc_tester.client import RMDBClient
 
 # %%
-os.environ["RMDB_PORT"] = "7003"
-client = RMDBClient(port=7003)
+os.environ["RMDB_PORT"] = "7005"
+client = RMDBClient(port=7005)
 client.connect()
+
+warehouse_df = pd.DataFrame(client.send_cmd("select * from warehouse;").data)
+district_df = pd.DataFrame(client.send_cmd("select * from district;").data)
+customer_df = pd.DataFrame(client.send_cmd("select * from customer;").data)
+new_orders_df = pd.DataFrame(client.send_cmd("select * from new_orders;").data)
+orders_df = pd.DataFrame(client.send_cmd("select * from orders;").data)
+order_line_df = pd.DataFrame(client.send_cmd("select * from order_line;").data)
+item_df = pd.DataFrame(client.send_cmd("select * from item;").data)
+stock_df = pd.DataFrame(client.send_cmd("select * from stock;").data)
+
+# %%
+
 result = client.send_cmd("select * from new_orders;")
 df = pd.DataFrame(result.data)
 print(len(df) - len(df.drop_duplicates()))
+duplicate_indices = df[df.duplicated(keep=False)].index.tolist()
+print(df.iloc[duplicate_indices[0], :])
 
 # %%
